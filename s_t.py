1f43d265cfdb8ace9315c2561d33c8b21bfbbd37
pedreopj <pelaezescobarpepo@gmail.com>
2024-08-22 07:07:29 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -90,18 +90,18 @@ if result:
     
     out_lang = st.selectbox(
         "Selecciona el lenguaje de salida",
-        ("Inglés", "Español", "Bengali", "Coreano", "Mandarín", "Japonés"),
+        ("Inglés", "Español", "Frances", "Coreano", "Catalan", "Japonés"),
     )
     if out_lang == "Inglés":
         output_language = "en"
     elif out_lang == "Español":
         output_language = "es"
-    elif out_lang == "Bengali":
-        output_language = "bn"
+    elif out_lang == "Frances":
+        output_language = "fr"
     elif out_lang == "Coreano":
         output_language = "ko"
-    elif out_lang == "Mandarín":
-        output_language = "zh-cn"
+    elif out_lang == "Catalan":
+        output_language = "ca"
     elif out_lang == "Japonés":
         output_language = "ja"
     
